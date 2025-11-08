5b3c2ab27ec3d70966869377c728c0df744656fd
root <root@favala.es>
2025-11-08 21:48:54 +0000

    Fix duplicated nav, remove nonexistent agenda link from layout.php

--- templates/layout.php
+++ templates/layout.php
@@ -13,24 +13,12 @@
   <div class="app">
     <header class="app-header">
       <h1>Gestor de Asociación</h1>
-    </header>
-    <nav class="app-nav">
-      <a href="index.php?page=dashboard">Inicio</a>
-      <a href="index.php?page=payments">Pagos</a>
-      <a href="index.php?page=vouchers">Vales</a>
-      <?php if($user && $user['role']==='admin'): ?>
-        <a href="index.php?page=users">Usuarios</a>
-        <a href="index.php?page=agenda">Agenda</a>
-      <?php endif; ?>
-      <a href="index.php?page=logout">Cerrar sesión</a>
-    </nav>
       <nav class="app-nav">
         <a href="index.php?page=dashboard">Inicio</a>
         <a href="index.php?page=payments">Pagos</a>
         <a href="index.php?page=vouchers">Vales</a>
         <?php if($user && $user['role']==='admin'): ?>
           <a href="index.php?page=users">Usuarios</a>
-          <a href="index.php?page=agenda">Agenda</a>
         <?php endif; ?>
         <a href="index.php?page=logout">Cerrar sesión</a>
       </nav>
